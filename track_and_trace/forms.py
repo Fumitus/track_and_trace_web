bd63c7292bdd8cb70f5e0867a0c2748950bdb56e
Fumitus <andrius.baikstis@biotecha.lt>
2019-01-27 20:30:03 +0200

    sugadinau

--- track_and_trace/forms.py
+++ track_and_trace/forms.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, EqualTo, Email, ValidationError
@@ -39,46 +38,4 @@ class ProductForm(FlaskForm):
     product_name = TextAreaField('Enter product name', validators=[DataRequired()])
     product_batch = TextAreaField('Enter product batch', validators=[DataRequired()])
     expire_date = TextAreaField('Enter product expire', validators=[DataRequired()])
-=======
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
-from wtforms.validators import DataRequired, Length, EqualTo, Email, ValidationError
-from track_and_trace.models import User
-
-class RegistrationForm(FlaskForm):
-    username = StringField('Username', 
-                        validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', 
-                        validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
-
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user:
-            raise ValidationError("That username is taken. Pleae choose another user name.")
-
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user:
-            raise ValidationError("That email is taken. Please choose another email.")
-
-class LoginForm(FlaskForm):
-    email = StringField('Email', 
-                        validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
-    submit = SubmitField('Login')
-
-class CodesForm(FlaskForm):
-    input_code = TextAreaField('Enter 2D code value from package', validators=[DataRequired()])
-    submit = SubmitField('Enter code')    
-
-class ProductForm(FlaskForm):
-    product_name = TextAreaField('Enter product name', validators=[DataRequired()])
-    product_batch = TextAreaField('Enter product batch', validators=[DataRequired()])
-    expire_date = TextAreaField('Enter product expire', validators=[DataRequired()])
->>>>>>> 05b65c03628311be22d7f5b2fbaf50c2b9c1cb3d
     submit = SubmitField('Enter code')
